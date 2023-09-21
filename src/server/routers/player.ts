cdf8933166823533c76c075a944bac0ab4423b34
thepeterkovacs <peterkov37@gmail.com>
2023-09-21 14:01:08 +0200

    change get is in game to private procedure

--- src/server/routers/player.ts
+++ src/server/routers/player.ts
@@ -3,16 +3,16 @@ import { TRPCError } from "@trpc/server"
 import { getDocumentFromUrl } from "@/lib/utils/server"
 
 import { GetIsInGame } from "../models/inputs"
-import { publicProcedure, router } from "../trpc"
+import { privateProcedure, router } from "../trpc"
 
-const getIsInGame = publicProcedure.input(GetIsInGame).query(async ({ input }) => {
+const getIsInGame = privateProcedure.input(GetIsInGame).query(async ({ input }) => {
 	const { region, name } = input
 
 	const url = `https://www.leagueofgraphs.com/summoner/${region}/${name}`
 	const document = await getDocumentFromUrl(url)
 
 	if (document.querySelector(".solo-text")) {
-		return new TRPCError({
+		throw new TRPCError({
 			code: "NOT_FOUND",
 			message: "Player with the given name and region does not exist",
 		})
