3b08648af8a07ff46d727190e01a067efbb4bfa1
thepeterkovacs <peterkov37@gmail.com>
2023-09-21 13:31:32 +0200

    add trpc auth middleware and private procedure

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,14 @@
 import { appRouter } from "@/server/appRouter"
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 
-const handler = (req: Request) =>
-	fetchRequestHandler({
+const handler = (req: Request) => {
+	const apiKey = req.headers.get("api-key") ?? ""
+
+	return fetchRequestHandler({
 		endpoint: "/api/trpc",
 		req,
 		router: appRouter,
-		createContext: () => ({}),
+		createContext: () => ({ apiKey }),
 	})
-
+}
 export { handler as GET, handler as POST }
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,26 @@
-import { initTRPC } from "@trpc/server"
+import { TRPCError, initTRPC } from "@trpc/server"
 
-const t = initTRPC.create()
+type Context = {
+	apiKey: string
+}
 
+const t = initTRPC.context<Context>().create()
 export const router = t.router
+
+export const auth = t.middleware(({ ctx, next }) => {
+	if (ctx.apiKey !== process.env.API_KEY) {
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "Invalid API key",
+		})
+	}
+
+	return next({
+		ctx: {
+			key: ctx.apiKey,
+		},
+	})
+})
+
 export const publicProcedure = t.procedure
+export const privateProcedure = t.procedure.use(auth)
