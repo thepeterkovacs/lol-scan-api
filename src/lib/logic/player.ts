1f9b80f5bb79f7791ac22c9d175b333a4e082fb0
thepeterkovacs <peterkov37@gmail.com>
2023-09-24 22:07:30 +0200

    jsdoc for player logic functions

--- src/lib/logic/player.ts
+++ src/lib/logic/player.ts
@@ -1,5 +1,15 @@
+import { Player } from "@/server/models/outputs"
+
 import { extractSubstring } from "../utils"
 
+/**
+ * Extracts the game mode from the provided HTML string.
+ * @param {string} html HTML string to extract the game mode from.
+ * @returns {string} The extracted game mode.
+ * @example
+ * const output = getGameMode('<h2 class="left relative"> ARAM <span id="gameDuration" ...')
+ * //output = "ARAM"
+ */
 export const getGameMode = (html: string): string => {
 	const prefix = '<h2 class="left relative">'
 	const suffix = '<span id="gameDuration"'
@@ -10,6 +20,14 @@ export const getGameMode = (html: string): string => {
 	return gameMode
 }
 
+/**
+ * Extracts the game duration in milliseconds from the provided HTML string.
+ * @param {string} html HTML string to extract the game duration from.
+ * @returns {number} The game duration in milliseconds.
+ * @example
+ * const output = getGameDuration(">(9:43)<")
+ * //output = 583000
+ */
 export const getGameDuration = (html: string): number => {
 	const prefix = ">("
 	const suffix = ")<"
@@ -23,10 +41,18 @@ export const getGameDuration = (html: string): number => {
 	return gameDuration
 }
 
-export const getPlayers = (html: string): { name: string }[] => {
+/**
+ * Extracts the players from the provided HTML string.
+ * @param {string} html HTML string to extract player names from.
+ * @returns {Player[]} An array of players.
+ * @example
+ * const output = getPlayers('data-summonername="Player1" data-summonerid ... data-summonername="Player2" data-summonerid')
+ * //output = [{ name: "Player1" }, { name: "Player2" }]
+ */
+export const getPlayers = (html: string): Player[] => {
 	const regex = /data-summonername="(.*)" data-summonerid/g
 
-	let players: { name: string }[] = []
+	let players: Player[] = []
 	let match: RegExpExecArray | null
 
 	while ((match = regex.exec(html)) !== null) {
--- src/server/models/outputs.ts
+++ src/server/models/outputs.ts
@@ -4,6 +4,8 @@ export const Player = z.object({
 	name: z.string(),
 })
 
+export type Player = z.infer<typeof Player>
+
 export const GetIsInGameOutput = z.object({
 	isInGame: z.boolean(),
 })
