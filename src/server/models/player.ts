e7f7d129227ef9d8a105dfbbdb811e60e1f8d448
thepeterkovacs <peterkov37@gmail.com>
2023-09-29 14:32:50 +0200

    rank property to player

--- src/server/models/player.ts
+++ src/server/models/player.ts
@@ -5,6 +5,7 @@ import { Region } from "./enums"
 export const Player = z.object({
 	name: z.string().describe("IGN (in game name) of the player."),
 	region: Region.describe("Name of the regional server where the player's account is based."),
+	rank: z.string().describe("Current rank of the player."),
 	isInGame: z.boolean().describe("Whether the player is currently in an active game."),
 })
 
