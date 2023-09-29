91977cb08edbfe2ceb030dcf38cbf7aaea5bad32
thepeterkovacs <peterkov37@gmail.com>
2023-09-29 12:19:25 +0200

    add live game get duration tests

--- tests/routers/liveGame.spec.ts
+++ tests/routers/liveGame.spec.ts
@@ -46,3 +46,50 @@ test.describe.parallel("getMode", () => {
 		expect(response.status() === 501 || response.status() === 200).toBeTruthy()
 	})
 })
+
+test.describe.parallel("getDuration", () => {
+	test("unauthorized_401", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getDuration`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": "",
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		expect(response.status()).toBe(401)
+	})
+
+	test("notFound_404", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getDuration`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":""}',
+			},
+		})
+
+		expect(response.status()).toBe(404)
+	})
+
+	test("success_200", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getDuration`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		expect(response.status() === 501 || response.status() === 200).toBeTruthy()
+	})
+})
