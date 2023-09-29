894817367413daaf262721b5df1da9dce9c30111
thepeterkovacs <peterkov37@gmail.com>
2023-09-29 12:45:59 +0200

    add live game get all data tests

--- tests/routers/liveGame.spec.ts
+++ tests/routers/liveGame.spec.ts
@@ -145,7 +145,7 @@ test.describe.parallel("getPlayers", () => {
 				"api-key": process.env.API_KEY,
 			},
 			params: {
-				input: '{"region":"eune","name":"Bardo"}',
+				input: '{"region":"eune","name":"Quicksave"}',
 			},
 		})
 
@@ -155,6 +155,59 @@ test.describe.parallel("getPlayers", () => {
 			const data = JSON.parse(await response.text()).result.data
 
 			expect(Array.isArray(data.players)).toBeTruthy()
+		}
+	})
+})
+
+test.describe.parallel("getAllData", () => {
+	test("unauthorized_401", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getAllData`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": "",
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		expect(response.status()).toBe(401)
+	})
+
+	test("notFound_404", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getAllData`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":""}',
+			},
+		})
+
+		expect(response.status()).toBe(404)
+	})
+
+	test("success_200", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.liveGame.getAllData`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		expect(response.status() === 501 || response.status() === 200).toBeTruthy()
+
+		if (response.status() === 200) {
+			const data = JSON.parse(await response.text()).result.data
+
+			expect(Object.prototype.toString.call(data)).toBe("[object Object]")
 		}
 	})
 })
