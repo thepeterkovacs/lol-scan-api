03304b85f78ba100100f5a9fc63b488e62f2d80a
thepeterkovacs <peterkov37@gmail.com>
2023-09-30 19:17:12 +0200

    add player get icon tests

--- tests/routers/player.spec.ts
+++ tests/routers/player.spec.ts
@@ -50,6 +50,56 @@ test.describe.parallel("getIsInGame", () => {
 	})
 })
 
+test.describe.parallel("getIcon", () => {
+	test("unauthorized_401", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.getIcon`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": "",
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		expect(response.status()).toBe(401)
+	})
+
+	test("notFound_404", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.getIcon`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":""}',
+			},
+		})
+
+		expect(response.status()).toBe(404)
+	})
+
+	test("success_200", async ({ request }) => {
+		const url = `${process.env.URL}/api/trpc/player.getIcon`
+
+		const response = await request.get(url, {
+			headers: {
+				"api-key": process.env.API_KEY,
+			},
+			params: {
+				input: '{"region":"eune","name":"Quicksave"}',
+			},
+		})
+
+		const data = JSON.parse(await response.text()).result.data
+
+		expect(response.status()).toBe(200)
+		expect(typeof data.icon).toBe("string")
+	})
+})
+
 test.describe.parallel("getAllData", () => {
 	test("unauthorized_401", async ({ request }) => {
 		const url = `${process.env.URL}/api/trpc/player.getAllData`
