756f001a4a26607f9fddfe07aa805c13fc661797
thepeterkovacs <peterkov37@gmail.com>
2023-10-11 23:13:10 +0200

    options to access control allow methods

--- next.config.js
+++ next.config.js
@@ -5,9 +5,18 @@ const nextConfig = {
 			{
 				source: "/:path*",
 				headers: [
-					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
-					{ key: "Access-Control-Allow-Methods", value: "GET" },
+					{
+						key: "Access-Control-Allow-Credentials",
+						value: "true",
+					},
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "*",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, OPTIONS",
+					},
 					{
 						key: "Access-Control-Allow-Headers",
 						value: "api-key, Authorization, Content-Type",
