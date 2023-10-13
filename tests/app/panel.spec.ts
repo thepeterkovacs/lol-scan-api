57e24ee289881b3b0ce354698e9a2ef34fb76b52
thepeterkovacs <peterkov37@gmail.com>
2023-10-13 14:24:09 +0200

    adapt app tests

--- tests/app/panel.spec.ts
+++ tests/app/panel.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from "@playwright/test"
 
 test("title_trpcPanel", async ({ page }) => {
-	await page.goto("http://localhost:3000")
+	await page.goto("http://localhost:3000/panel")
 
 	await expect(page).toHaveTitle("tRPC.panel()")
 })
